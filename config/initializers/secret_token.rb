ecc8d728b1b77d7bb107ee3f7cffb425cde411fe
DenysDenylson <DenysDenylson@gmail.com>
2014-04-30 11:49:01 -0400

    prueba commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyPrueba::Application.config.secret_key_base = '8c16a9f26f1c13b00a5a88eda561f2750edc9231834b65d52ca93dbf2f9e480261ad857c6901ef8e597aacb4a8f6bb47f92f4a67da96041875e3a3eb7fd509c5'
